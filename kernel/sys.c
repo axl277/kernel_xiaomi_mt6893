6f4520ccc022a014bbc50e39a7aaa73a9132eea2
Axlorin° <154013417+axl277@users.noreply.github.com>
2026-04-03 12:55:23 +0700

    Uname

--- kernel/sys.c
+++ kernel/sys.c
@@ -1191,12 +1191,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-    if (!strncmp(current->comm, "bpfloader", 9) ||
-	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4) ||
-	    !strncmp(current->comm, "uprobestats", 11)) {
+    if (current_uid().val == 0 && 
+		(!strncmp(current->comm, "bpfloader", 9) ||
+		!strncmp(current->comm, "netbpfload", 10) ||
+	    	!strncmp(current->comm, "netd", 4))) {
 		strcpy(tmp.release, "5.10.246");
-		pr_debug("fake uname: %s release=%s\n",
+		pr_info("fake uname: %s/%d release=%s\n",
 			 current->comm, tmp.release);
 	}
 	up_read(&uts_sem);
