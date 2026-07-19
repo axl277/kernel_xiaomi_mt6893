a89616c5ab8be4e1835b7b929975a357ebc7a0cf
Axlorin° <154013417+axl277@users.noreply.github.com>
2026-07-19 22:43:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ SECONDS=0
 DATE=$(date '+%Y%m%d-%H%M')
 
 # Device
-DEVICE="${1:-chopin}"
+DEVICE="${1:-Chopin}"
 DEFCONFIG="${DEVICE}_defconfig"
 ZIPNAME="Artic-Core-Resukisu-KPatch-${DEVICE}-${DATE}.zip"
 
